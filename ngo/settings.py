34b5364d69cdbf5f07208c54dc6a1085e5dd1192
vidya365 <vidya@itegoss.in>
2025-12-30 15:03:48 +0530

    temporarily allow all hosts

--- ngo/settings.py
+++ ngo/settings.py
@@ -13,11 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    ".up.railway.app",
-]
+ALLOWED_HOSTS = ["*"]
 
 # ===================== APPLICATIONS =====================
 INSTALLED_APPS = [
