c62b0e9d8fd30e87a238db9e765513f18aed2e1a
vidya365 <vidya@itegoss.in>
2026-01-06 10:54:02 +0530

    CSRF

--- ngo/settings.py
+++ ngo/settings.py
@@ -23,6 +23,7 @@ DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://web-production-c7334.up.railway.app","http://127.0.0.1:8000"]
 
 
 # ===================== APPLICATIONS =====================
